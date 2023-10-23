daf12146940c1e430c4999b4ee15b12f2ae64eb9
Ralph Estor <ralphestor30@gmail.com>
2023-10-23 22:23:18 +0800

    small update on pages

--- src/app/connect/page.tsx
+++ src/app/connect/page.tsx
@@ -1,9 +1,13 @@
 const Connect = () => {
-    return ( 
-        <main>
-            
-        </main>
-     );
-}
- 
+  return (
+    <main className="w-full h-[100vh] min-h-[650px] bg-[#040E26]">
+      <header className="w-full h-[300px] bg-[#620000] flex justify-center items-center">
+        <h1 className="text-white font-black text-[50px] text-shadow">
+          Connect
+        </h1>
+      </header>
+    </main>
+  );
+};
+
 export default Connect;
